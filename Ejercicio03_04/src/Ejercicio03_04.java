2d53b147f2394b06cfce52d1824197f850526465
Alejandro <capucha1990@gmail.com>
2016-11-24 17:54:40 +0100

    ejercicio03

---
+++ Ejercicio03_04/src/Ejercicio03_04.java
@@ -0,0 +1,28 @@
+/*
+ * Programa que lea una frase y una palabra y que nos diga
+ *  la posición de dicha palabra en la frase y si no la
+ *   encuentra devuelva un cero.
+ */
+import java.util.Scanner;
+public class Ejercicio03_04 {
+
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+		
+		
+		
+		
+		System.out.print("Introduce una frase: ");
+		frase=teclado.nextLine();
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
